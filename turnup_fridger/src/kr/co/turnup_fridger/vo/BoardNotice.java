c86d13ef3740c702d51782f377c4ccfb65d79a33
jangkuok <jangkuok@naver.com>
2017-07-01 19:07:05 +0900

    공지사항 파트 3차

--- turnup_fridger/src/kr/co/turnup_fridger/vo/BoardNotice.java
+++ turnup_fridger/src/kr/co/turnup_fridger/vo/BoardNotice.java
@@ -3,67 +3,74 @@ package kr.co.turnup_fridger.vo;
 import java.io.Serializable;
 import java.util.Date;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import kr.co.turnup_fridger.util.DateJSONSerializer;
+
 
 
 public class BoardNotice implements Serializable
 {
-	private int boardNoticeId;
-	private String boardNoticeItems;
-	private String boardNoticeTitle;
-	private String boardNoticeTxt;
-	private String boardNoticeImg;
+	private int id;
+	private String items;
+	private String title;
+	private String txt;
+	private String img;
+	@JsonSerialize(using=DateJSONSerializer.class)
 	private Date date;
 	
 	public BoardNotice (){}
 
-	public BoardNotice(int boardNoticeId, String boardNoticeItems, String boardNoticeTitle, String boardNoticeTxt,
-			String boardNoticeImg, Date date) {
-		this.boardNoticeId = boardNoticeId;
-		this.boardNoticeItems = boardNoticeItems;
-		this.boardNoticeTitle = boardNoticeTitle;
-		this.boardNoticeTxt = boardNoticeTxt;
-		this.boardNoticeImg = boardNoticeImg;
+	public BoardNotice(int id, String items, String title, String txt, String img, Date date) {
+		super();
+		this.id = id;
+		this.items = items;
+		this.title = title;
+		this.txt = txt;
+		this.img = img;
 		this.date = date;
 	}
 
-	public int getBoardNoticeId() {
-		return boardNoticeId;
+	public int getId() {
+		return id;
 	}
 
-	public void setBoardNoticeId(int boardNoticeId) {
-		this.boardNoticeId = boardNoticeId;
+	public void setId(int id) {
+		this.id = id;
 	}
 
-	public String getBoardNoticeItems() {
-		return boardNoticeItems;
+	public String getItems() {
+		return items;
 	}
 
-	public void setBoardNoticeItems(String boardNoticeItems) {
-		this.boardNoticeItems = boardNoticeItems;
+	public void setItems(String items) {
+		this.items = items;
 	}
 
-	public String getBoardNoticeTitle() {
-		return boardNoticeTitle;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setBoardNoticeTitle(String boardNoticeTitle) {
-		this.boardNoticeTitle = boardNoticeTitle;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
-	public String getBoardNoticeTxt() {
-		return boardNoticeTxt;
+	public String getTxt() {
+		return txt;
 	}
 
-	public void setBoardNoticeTxt(String boardNoticeTxt) {
-		this.boardNoticeTxt = boardNoticeTxt;
+	public void setTxt(String txt) {
+		this.txt = txt;
 	}
 
-	public String getBoardNoticeImg() {
-		return boardNoticeImg;
+	public String getImg() {
+		return img;
 	}
 
-	public void setBoardNoticeImg(String boardNoticeImg) {
-		this.boardNoticeImg = boardNoticeImg;
+	public void setImg(String img) {
+		this.img = img;
 	}
 
 	public Date getDate() {
@@ -76,21 +83,20 @@ public class BoardNotice implements Serializable
 
 	@Override
 	public String toString() {
-		return "BoardNotice [boardNoticeId=" + boardNoticeId + ", boardNoticeItems=" + boardNoticeItems
-				+ ", boardNoticeTitle=" + boardNoticeTitle + ", boardNoticeTxt=" + boardNoticeTxt + ", boardNoticeImg="
-				+ boardNoticeImg + ", date=" + date + "]";
+		return "BoardNotice [id=" + id + ", items=" + items + ", title=" + title + ", txt=" + txt + ", img=" + img
+				+ ", date=" + date + "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + boardNoticeId;
-		result = prime * result + ((boardNoticeImg == null) ? 0 : boardNoticeImg.hashCode());
-		result = prime * result + ((boardNoticeItems == null) ? 0 : boardNoticeItems.hashCode());
-		result = prime * result + ((boardNoticeTitle == null) ? 0 : boardNoticeTitle.hashCode());
-		result = prime * result + ((boardNoticeTxt == null) ? 0 : boardNoticeTxt.hashCode());
 		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((img == null) ? 0 : img.hashCode());
+		result = prime * result + ((items == null) ? 0 : items.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((txt == null) ? 0 : txt.hashCode());
 		return result;
 	}
 
@@ -103,34 +109,36 @@ public class BoardNotice implements Serializable
 		if (getClass() != obj.getClass())
 			return false;
 		BoardNotice other = (BoardNotice) obj;
-		if (boardNoticeId != other.boardNoticeId)
-			return false;
-		if (boardNoticeImg == null) {
-			if (other.boardNoticeImg != null)
-				return false;
-		} else if (!boardNoticeImg.equals(other.boardNoticeImg))
-			return false;
-		if (boardNoticeItems == null) {
-			if (other.boardNoticeItems != null)
-				return false;
-		} else if (!boardNoticeItems.equals(other.boardNoticeItems))
-			return false;
-		if (boardNoticeTitle == null) {
-			if (other.boardNoticeTitle != null)
-				return false;
-		} else if (!boardNoticeTitle.equals(other.boardNoticeTitle))
-			return false;
-		if (boardNoticeTxt == null) {
-			if (other.boardNoticeTxt != null)
-				return false;
-		} else if (!boardNoticeTxt.equals(other.boardNoticeTxt))
-			return false;
 		if (date == null) {
 			if (other.date != null)
 				return false;
 		} else if (!date.equals(other.date))
 			return false;
+		if (id != other.id)
+			return false;
+		if (img == null) {
+			if (other.img != null)
+				return false;
+		} else if (!img.equals(other.img))
+			return false;
+		if (items == null) {
+			if (other.items != null)
+				return false;
+		} else if (!items.equals(other.items))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (txt == null) {
+			if (other.txt != null)
+				return false;
+		} else if (!txt.equals(other.txt))
+			return false;
 		return true;
 	}
+
+
 
 }
