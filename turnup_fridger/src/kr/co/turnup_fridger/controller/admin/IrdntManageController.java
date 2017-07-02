55e6eb8c6255cc500afcac374b1de2a14a0a5bb7
park ys <kite_90@naver.com>
2017-07-03 00:43:55 +0900

    재료관리 핸들러

--- turnup_fridger/src/kr/co/turnup_fridger/controller/admin/IrdntManageController.java
+++ turnup_fridger/src/kr/co/turnup_fridger/controller/admin/IrdntManageController.java
@@ -7,22 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import kr.co.turnup_fridger.service.IrdntManageService;
 import kr.co.turnup_fridger.vo.IrdntManage;
 
 @Controller
+@RequestMapping("/irdntManage/")
 public class IrdntManageController {
 
 	@Autowired
 	private IrdntManageService service;
 	
-	@RequestMapping("register")
-	public String resisterIrdnt(@ModelAttribute ("IrdntManage") @Valid IrdntManage irdntManage, BindingResult errors){
-		
-		
+	@RequestMapping("createIrdnt")
+	public ModelAndView createIrdnt(@ModelAttribute ("IrdntManage") @Valid IrdntManage irdntManage, BindingResult errors){
+	
 		if(errors.hasErrors()){
-			return "/admin/Irdnt_register_form";
+			return new ModelAndView("irdntManage/Irdnt_create_form");
 		}
 		
 		try {
@@ -31,8 +33,25 @@ public class IrdntManageController {
 			//중복으로 던져진 에러메시지 받아야한다. 
 			
 		}
-		return "/admin/Irdnt_register_success";
+		return new ModelAndView("irdntManage/Irdnt_create_success");
 	}
 	
+	@RequestMapping("updateIrdnt")
+	public ModelAndView updateIrdnt(@ModelAttribute IrdntManage irdnt, BindingResult errors) throws Exception{
+		
+		service.updateIrdnt(irdnt);
+		
+		return new ModelAndView("irdntManage/Irdnt_update_success");
+	}
+	
+	@RequestMapping("removeIrdnt")
+	public ModelAndView removeIrdnt(@RequestParam int irdntId) throws Exception{
+		service.removeIrdnt(irdntId);
+		
+		
+		return new ModelAndView("irdntManage/Irdnt_list");
+	}
+	
+
 	
 }
