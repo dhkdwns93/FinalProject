70dd89375d85635d58716f5613b2ef8d7a0093fd
Seo Hyeonhwa <qms1109@hanmail.net>
2017-07-19 19:57:22 +0900

    commit!

--- turnup_fridger/src/kr/co/turnup_fridger/util/InsertMain.java
+++ turnup_fridger/src/kr/co/turnup_fridger/util/InsertMain.java
@@ -4,16 +4,22 @@ public class InsertMain {
 	public static void main(String[] args) {
 		InsertMethod insertMethod=new InsertMethod();
 		
+		//레시피기본정보
+		//insertMethod.insertAPI("C:\\java\\api\\recipe\\","recipe_info");
+		
+		//레시피과정정보 
+		//insertMethod.insertAPI("C:\\java\\api\\recipe\\","recipe_crse");
 		
 		//레시피재료정보
+
 		//insertMethod.insertAPI("C:\\Java\\api\\recipe\\","recipe_irdnt");//(파일루트, txt파일명)
 
-		
 		//레시피기본정보
 		//insertMethod.insertAPI("C:\\Java\\api\\recipe\\","recipe_info");
-		
+
+
 		//재료관리 
-		//insertMethod.insertAPI("C:\\Java\\api\\recipe\\","irdnt_manage");//(파일루트, txt파일명)
+		insertMethod.insertAPI("C:\\java\\api\\recipe\\","irdnt_manage");//(파일루트, txt파일명)
 
 		
 		
@@ -22,12 +28,15 @@ public class InsertMain {
 		///////은영이가 //////////////////
 		//레시피재료정보
 		//insertMethod.insertAPI("D:\\Eunyoung\\java\\api\\","recipe_irdnt");//(파일루트, txt파일명)
-		insertMethod.insertAPI("D:\\Eunyoung\\java\\api\\","irdnt_manage");//(파일루트, txt파일명)
+		//insertMethod.insertAPI("C:\\java\\api\\recipe","irdnt_manage");//(파일루트, txt파일명)
 		
 		//레시피기본정보
 		//insertMethod.insertAPI("D:\\Eunyoung\\java\\api\\","recipe_info");
+
+		//insertMethod.insertAPI("C:\\Java\\api\\recipe\\","recipe_crse");
+
 		
-		//재료관리 
-		//insertMethod.insertAPI("D:\\Eunyoung\\java\\api\\","recipe_crse");//(파일루트, txt파일명)
+		
+
 	}//end of main
 }
